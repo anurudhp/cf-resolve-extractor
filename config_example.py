d1a6e644df70be1abe321c6bf4ac42435e83e7d5
Anurudh Peduri <7265746+anurudhp@users.noreply.github.com>
2022-01-15 01:46:58 +0530

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -2,10 +2,11 @@ api_key = 'YOUR CF API KEY HERE'
 secret = 'YOUR CF API SECRET KEY'
 
 # ID of cf contest
-contest_id = '331059'
+contest_id = 'CONTEST_ID'
+
 # list of regions/award groups. Each team belongs to at most one region
 regions = []
 
+# return (gold, silver, bronze) counts
 def medal_counts(num_teams):
-    k = (num_teams + 14) // 15
-    return (k, k, k)
+    return (4, 4, 4)
